9ef8fb115aefc76393aecfd69de03cdadee0b55e
maneesha <ahseenam@gmail.com>
2016-05-13 21:48:04 -0400

    add installed apps

--- elections/settings.py
+++ elections/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'phlcitycouncil.apps.PhlcitycouncilConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
