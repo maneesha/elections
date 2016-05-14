aa6550b89f20e8f1dff5ea5830a5f97f8f2cd8d2
maneesha <ahseenam@gmail.com>
2016-05-14 13:13:26 -0400

    fix null allowed in Person model

--- phlcitycouncil/admin.py
+++ phlcitycouncil/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Person, Candidate, District, Election, Vote
+
+admin.site.register([Person, Candidate, District, Election, Vote])
+
+
--- phlcitycouncil/models.py
+++ phlcitycouncil/models.py
@@ -5,11 +5,11 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length = 50)
     middle_name = models.CharField(max_length = 50, null = True, blank = True)
-    last_name = models.CharField(max_length = 50, null = True, blank = True)
-    birthdate = models.DateTimeField()
-    race = models.CharField(max_length = 20)
-    gender = models.CharField(max_length = 6)
-    notes = models.TextField()
+    last_name = models.CharField(max_length = 50)
+    birthdate = models.DateTimeField(null = True, blank = True)
+    race = models.CharField(max_length = 20, null = True, blank = True)
+    gender = models.CharField(max_length = 6, null = True, blank = True)
+    notes = models.TextField(null = True, blank = True)
 
     def __str__(self):
         return '%s, %s' % (self.first_name, self.last_name)
