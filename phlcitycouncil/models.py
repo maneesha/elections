1ae7b77131560634ca632e7f833c2acc632f7d4e
maneesha <ahseenam@gmail.com>
2016-05-14 13:27:27 -0400

    create Term model

--- phlcitycouncil/models.py
+++ phlcitycouncil/models.py
@@ -25,15 +25,29 @@ class District(models.Model):
     district_number = models.CharField(max_length = 10)
     state = models.CharField(max_length = 2)
 
+class Office(models.Model):
+    office = models.CharField(max_length = 50)
+
 class Election(models.Model):
     district = models.ForeignKey(District, on_delete=models.CASCADE)
     election_date = models.DateTimeField()
-    office = models.CharField(max_length = 50)
+    office = models.ForeignKey(Office, on_delete=models.CASCADE)
 
 class Vote(models.Model):
     candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
     election = models.ForeignKey(Election, on_delete=models.CASCADE)
     vote_count = models.IntegerField(null = True, blank = True)
+
+class Term(models.Model):
+    candidate = models.ForeignKey(Candidate, on_delete=models.CASCADE)
+    office = models.ForeignKey(Office, on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    effective_start_year = models.IntegerField()
+    effecive_end_year = models.IntegerField()
+    departed = models.CharField(max_length = 25)
+    notes = models.TextField()
+
 
 
 
