033244d616d05f18bbf369020dc85f4512f0e258
maneesha <ahseenam@gmail.com>
2016-05-20 21:00:16 -0400

    create column view for Person

--- phlcitycouncil/admin.py
+++ phlcitycouncil/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
+from django.db import models
+
 
 # Register your models here.
 
 from .models import Person, Candidate, District, Election, Vote, Term, Office
 
-admin.site.register([Person, Candidate, District, Election, Vote, Term, Office])
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birthdate', 'gender')
 
-
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Candidate)
+admin.site.register(District)
+admin.site.register(Election)
+admin.site.register(Vote)
+admin.site.register(Term)
+admin.site.register(Office)
