c3448827039a446d82ebdb1e3d509f3e61e52640
maneesha <ahseenam@gmail.com>
2016-05-20 21:49:10 -0400

    add eff start/end date to Term Admin

--- phlcitycouncil/admin.py
+++ phlcitycouncil/admin.py
@@ -9,10 +9,13 @@ from .models import Person, Candidate, District, Election, Vote, Term, Office
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'birthdate', 'gender')
 
+class TermAdmin(admin.ModelAdmin):
+    list_display = ('candidate', 'start_date', 'end_date', 'eff_start_year', 'eff_end_year')
+
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Candidate)
 admin.site.register(District)
 admin.site.register(Election)
 admin.site.register(Vote)
-admin.site.register(Term)
+admin.site.register(Term, TermAdmin)
 admin.site.register(Office)
