bf04c71257929da0b84912b189c959ee8a41e419
maneesha <ahseenam@gmail.com>
2016-11-10 13:04:28 -0500

    create view for sum of votes

--- phlcitycouncil/views.py
+++ phlcitycouncil/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
+from django.template import loader
 from .models import Person, District, Election, Candidate, Vote
 
 from django.db.models import Count, Sum
@@ -10,16 +11,19 @@ def index(request):
     # Return some ordinary text
     # return HttpResponse("Hello, main page.")
 
-    # sum_votes = Vote.objects.all().values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name').annotate(Sum('vote_count'))
+    sum_votes = Vote.objects.all().values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name').annotate(Sum('vote_count'))
 
     # return HttpResponse(sum_votes)
 
-    elections = Election.objects.all()
-    output =  ", ".join([q.district.ward + q.district.division + str(q.election_date) + q.office.office for q in elections])
+    # elections_list = Election.objects.all().order_by("district__ward")
+    # template = loader.get_template('phlcitycouncil/index.html')
+    context = {
+        "sum_votes":sum_votes,
+    }
+    # output =  ", ".join([q.district.ward + q.district.division + str(q.election_date) + q.office.office for q in elections])
 
-
-    return HttpResponse(output)
-
+    # return render(request, 'phlcitycouncil/index.html', context)
+    return render(request, 'phlcitycouncil/index.html', context)
 
 
 def phlcitycouncil(request):
