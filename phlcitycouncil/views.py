e2842be4b81e78230311cc8ebdf83ccc008d1c03
maneesha <ahseenam@gmail.com>
2016-11-10 10:33:01 -0500

    playing around with getting data in views

--- phlcitycouncil/views.py
+++ phlcitycouncil/views.py
@@ -1,10 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Person, District, Election, Candidate, Vote
+
+from django.db.models import Count, Sum
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, main page.")
+    # Return some ordinary text
+    # return HttpResponse("Hello, main page.")
+
+    # sum_votes = Vote.objects.all().values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name').annotate(Sum('vote_count'))
+
+    # return HttpResponse(sum_votes)
+
+    elections = Election.objects.all()
+    output =  ", ".join([q.district.ward + q.district.division + str(q.election_date) + q.office.office for q in elections])
+
+
+    return HttpResponse(output)
+
+
 
 def phlcitycouncil(request):
     return HttpResponse("vistiing phlcitycouncil")
