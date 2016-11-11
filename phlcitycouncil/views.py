b35a326658a774177c3654dd7dcebe5564c61ed1
maneesha <ahseenam@gmail.com>
2016-11-11 10:14:10 -0500

    view vote counts by office and sorted

--- phlcitycouncil/views.py
+++ phlcitycouncil/views.py
@@ -16,14 +16,17 @@ def vote_count(request):
     # Return some ordinary text
     # return HttpResponse("Hello, main page.")
 
-    sum_votes = Vote.objects.all().values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name').annotate(Sum('vote_count'))
+    sum_votes_by_candidate = Vote.objects.all().values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name', 'candidate__party').annotate(total_votes = Sum('vote_count')).order_by('-total_votes')
 
+
+    sum_votes_by_office = Vote.objects.all().values('election__office__office').annotate(Sum('vote_count'))
     # return HttpResponse(sum_votes)
 
     # elections_list = Election.objects.all().order_by("district__ward")
     # template = loader.get_template('phlcitycouncil/index.html')
     context = {
-        "sum_votes":sum_votes,
+        "sum_votes_by_candidate":sum_votes_by_candidate,
+        "sum_votes_by_office":sum_votes_by_office
     }
     # output =  ", ".join([q.district.ward + q.district.division + str(q.election_date) + q.office.office for q in elections])
 
