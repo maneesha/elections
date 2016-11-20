b9f69e7b06c7f85a89e03dca4e7596081e8e5a4c
maneesha <ahseenam@gmail.com>
2016-11-20 13:50:38 -0500

    merge conflict in fix str rep for Term

--- phlcitycouncil/models.py
+++ phlcitycouncil/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+from django.db.models import Q
+
+from django.core.exceptions import ValidationError
+
+
 # Create your models here.
 
 class Person(models.Model):
@@ -111,8 +116,110 @@ class Term(models.Model):
     departed = models.CharField(max_length = 25)
     notes = models.TextField(null = True, blank = True)
 
+
+    ## ADD UNIQUE TOGETHER CLAUSE  ('candidate', 'office', 'start_date', 'end_date')
+    ## ADD CHOICES FOR departed 
+
+
+    def overlapping_terms(self):
+        # # Raise error if: 
+        # # This candidate's start date for this office < this candidate's end date for another term/office  
+        # # This candidate's end date for this office > this candidate's start date for another term/office
+        # # This candidate's start date for this office < another candidate's end date for this office
+        # # This candidate's end date for this office > another candidate's start date for this office 
+        # # negate does it not overlap - may be easier than checking they do
+        # print("THE CREATED START DATE IS: ", self.start_date)
+        # print("THE CREATED END DATE IS: ", self.end_date)
+        # print("THE CREATED CANDIDATE IS: ", self.candidate_id, self.candidate)
+        # print("THE CREATED OFFICE IS: ", self.office_id, self.office)
+        # # print("OBJECTS WHERE START DATE IS GREATER THAN SELF START DATE")
+        # #  print(Term.objects.filter(start_date__lte = self.start_date))
+        # print("##########")
+
+
+        # # Check to see if that candidate has any overlapping dates
+        # if Term.objects.filter(Q(candidate_id = self.candidate_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)).exists():
+
+        #     print("THERE IS AN ERROR WITH OVERLAPPING DATES FOR THIS CANDIDATE")
+        #     print(Term.objects.filter(Q(candidate_id = self.candidate_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)))
+
+        #     xx = Term.objects.filter(Q(candidate_id = self.candidate_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)).exists()
+        #     print(xx)
+
+
+        #     print("END OVERLAPPING CANDIDATE ERROR")
+        #     # print("### FILTERED Q: ", Term.objects.filter(Q(candidate_id = self.candidate_id)), "#### END")
+
+        # # Check to see if that office has any overlapping dates
+        # if Term.objects.filter(Q(office_id = self.office_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)).exists():
+
+        #     print("THERE IS AN ERROR WITH OVERLAPPING DATES FOR THIS OFFICE")
+        #     print(Term.objects.filter(Q(office_id = self.office_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)))
+        #     print("END OVERLAPPING OFFICE ERROR")
+
+
+
+        if Term.objects.filter(Q(candidate_id = self.candidate_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)).exists():
+
+            print("this is where validation error should go")
+            raise ValidationError("Overlapping dates for Candidate")
+
+        if Term.objects.filter(Q(office_id = self.office_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)).exists():
+            
+            print("this is where validation error should go")
+            raise ValidationError("Overlapping dates for Office")
+
+
+        return self.candidate, self.office, self.start_date, self.end_date
+
+
+
+    def clean(self):
+
+        try:
+            self.overlapping_terms()
+        except ValidationError:
+            print("### THIS IS A VALIDATION ERROR ###")
+
+        # if Term.objects.filter(Q(candidate_id = self.candidate_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)).exists():
+
+        #     print("this is where validation error should go")
+        #     raise ValidationError("Overlapping dates for Candidate")
+
+        # if Term.objects.filter(Q(office_id = self.office_id)).filter(Q(start_date__gt=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lt=self.end_date)).exists():
+            
+        #     print("this is where validation error should go")
+        #     raise ValidationError("Overlapping dates for Office")
+
+        # if Term.objects.filter(Q(start_date__gte=self.start_date, start_date__lt=self.end_date) | Q(end_date__gt=self.start_date, end_date__lte=self.end_date)).exists():
+        #     raise ValidationError("Overlapping dates")
+
+        # pass
+        
+
+    
+
+    def test_method(self):
+        # Get all of this candidate's records
+        q = Term.objects.all().filter(Q(candidate_id = self.candidate_id)).filter(Q(start_date__lt = self.start_date))
+
+        print("This is the test method")        
+        print(self.candidate)
+
+        print("another line")
+        print("query: ", q)
+        print("last line")
+
+
+        # pass
+
+
+    # def clean():
+    #     pass
+
     def __str__(self):
         return '%s, %s, start: %s, end: %s, %s' % (self.candidate, self.office, self.start_date, self.end_date, self.departed)
+
 
 
 
