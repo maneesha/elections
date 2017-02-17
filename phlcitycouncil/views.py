da34b3a2d1203801c57e8c83c1834412bd23ca84
maneesha sane <maneeshasane@gmail.com>
2017-02-16 20:49:59 -0500

    display separeted rep & dem at large counts

--- phlcitycouncil/views.py
+++ phlcitycouncil/views.py
@@ -13,20 +13,28 @@ def index(request):
 
 
 def vote_count(request):
-    # Return some ordinary text
-    # return HttpResponse("Hello, main page.")
-
     sum_votes_by_candidate = Vote.objects.all().values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name', 'candidate__party').annotate(total_votes = Sum('vote_count')).order_by('-total_votes')
+
+    # Retrieve At Large Republican candidates and votes
+    sum_votes_by_candidate_atlarge_dems = Vote.objects.filter(election__office__office='COUNCIL AT LARGE', candidate__party="Democrat").values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name', 'candidate__party').annotate(total_votes = Sum('vote_count')).order_by('-total_votes')
+
+    # Retrive At Large Democrate candidates and votes
+    sum_votes_by_candidate_atlarge_reps = Vote.objects.filter(election__office__office='COUNCIL AT LARGE', candidate__party="Republican").values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name', 'candidate__party').annotate(total_votes = Sum('vote_count')).order_by('-total_votes')
+
+    # sum_votes_by_candidate_district = Vote.objects.filter(election__office__office__starts_with='DIST', candidate__party="Republican").values('election__office__office',  'candidate__person__last_name', 'candidate__person__first_name', 'candidate__party').annotate(total_votes = Sum('vote_count')).order_by('-total_votes')
+
 
 
     sum_votes_by_office = Vote.objects.all().values('election__office__office').annotate(Sum('vote_count'))
-    # return HttpResponse(sum_votes)
 
-    # elections_list = Election.objects.all().order_by("district__ward")
-    # template = loader.get_template('phlcitycouncil/index.html')
+
     context = {
         "sum_votes_by_candidate":sum_votes_by_candidate,
-        "sum_votes_by_office":sum_votes_by_office
+        "sum_votes_by_office":sum_votes_by_office,
+        "sum_votes_by_candidate_atlarge_dems":sum_votes_by_candidate_atlarge_dems,
+        "sum_votes_by_candidate_atlarge_reps":sum_votes_by_candidate_atlarge_reps,
+        # "sum_votes_by_candidate_district":sum_votes_by_candidate_district,
+
     }
     # output =  ", ".join([q.district.ward + q.district.division + str(q.election_date) + q.office.office for q in elections])
 
